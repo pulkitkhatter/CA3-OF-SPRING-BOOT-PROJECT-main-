8894853aef9e2b48463fedf3125ab6a7c51d6d2a
Pulkit Khatter <84480808+pulkitkhatter@users.noreply.github.com>
2023-11-18 23:29:14 +0530

    Update AppSecurityConfig.java

--- JavaProject/JavaProject/src/main/java/com/example/JavaProject/AppSecurityConfig.java
+++ JavaProject/JavaProject/src/main/java/com/example/JavaProject/AppSecurityConfig.java
@@ -14,17 +14,25 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import java.util.ArrayList;
 import java.util.List;
 
-@Configuration
-@EnableWebSecurity
-public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
-
     @Bean
     @Override
-    protected UserDetailsService userDetailsService(){
-        List<UserDetails> users=new ArrayList<>();
+    protected UserDetailsService userDetailsService() {
+        List<UserDetails> users = new ArrayList<>();
         users.add(User.withDefaultPasswordEncoder().username("pulkit").password("1234").roles("USER").build());
-
         users.add(User.withDefaultPasswordEncoder().username("rohit").password("1234").roles("USER").build());
+        users.add(User.withDefaultPasswordEncoder().username("admin").password("admin123").roles("ADMIN").build());
         return new InMemoryUserDetailsManager(users);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api/create").hasRole("ADMIN")
+                .antMatchers("/api/put", "/api/post", "/api/get").hasRole("ADMIN")
+                .antMatchers("/api/getAll").authenticated() // Assumes all authenticated users can access
+                .and()
+                .formLogin().permitAll() // Customize login if needed
+                .and()
+                .logout().permitAll(); // Customize logout if needed
     }
 }
